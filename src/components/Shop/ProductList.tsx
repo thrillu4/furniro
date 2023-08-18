78ee1355f125eab7ba823602739f4bcfa9c794ab
Kraiovyi Denys <tonopik@gmail.com>
2023-08-18 15:35:14 +0300

    fix bug with hover effect in product list for mobile and desktop versions, make a notification for users when they click on actions

--- src/components/Shop/ProductList.tsx
+++ src/components/Shop/ProductList.tsx
@@ -23,7 +23,9 @@ interface ProductListProps {
 
 const ProductList: React.FC<ProductListProps> = ({currentPage, productsPerPage, handleAddToCart, handleAddToComparing, handleAddToFavorite, sortedProducts}) => {
 const [isOpenShareModal, setOpenShareModal] = useState(false);
-const [copied, setCopied] = useState(false);
+const [copied, setCopied] = useState('');
+const [isOpenDescrMenu, setOpenDescrMenu] = useState(false);
+const [activeProductId, setActiveProductId] = useState('');
 
 
 const lastPostIndex = currentPage * productsPerPage;
@@ -33,16 +35,22 @@ const url = window.location.href
 
 const toggleOpenModal = () => {
     setOpenShareModal(!isOpenShareModal)
-    navigator.clipboard.writeText
 }
 
-const handleCopyLink = () => {
+const handleCopyLink = (str: string) => {
     navigator.clipboard.writeText(url).then(() => {
-        setCopied(true)
+        setCopied(str)
         setTimeout(() => {
-            setCopied(false)
+            setCopied('')
         }, 3000)
     })
+}
+
+const handleOpenDescrMenu = (id: string) => {
+    if(!isOpenDescrMenu) {
+        setOpenDescrMenu(true)
+    }
+    setActiveProductId(id);
 }
 
   return (
@@ -57,7 +65,8 @@ const handleCopyLink = () => {
             whileInView='visible'
             viewport={{amount: 0.2}}
             custom={`0.${index}`}
-            key={id} className="product-list-item relative ">
+            onClick={() => handleOpenDescrMenu(id)}
+            key={id} className="product-list-item relative">
                 <img className="w-full md:h-auto" src={image} alt="product" />
                 <div className='absolute top-1 right-1 md:top-6  md:right-6'>
                     {promotional && <div className='text-white bg-red-400 text-[8px] md:text-base rounded-full md:py-3 p-1 md:px-2 font-semibold'>{percent}</div> || newProduct && <div className='text-white bg-teal-500 text-[8px] md:text-base rounded-full md:py-3 md:px-2 p-1 font-semibold'>New</div>}
@@ -69,9 +78,14 @@ const handleCopyLink = () => {
                     <div className="font-semibold text-[10px] md:text-base lg:text-xl">{promotional ? promotionalPrice : price}</div>
                     <div className="text-zinc-400 text-[10px]  md:text-[14px] lg:text-[16px] line-through">{promotional ? price : promotionalPrice}</div>
                 </div>
-                </div>
-                <div className="absolute inset-0 bg-gray-600 opacity-0  hover:opacity-75 transition-opacity  text-white font-semibold">
-                    <button onClick={() => handleAddToCart(currentProduct)} className="hover:text-white hover:bg-orange-500 transition-all duration-300 my-0 mx-auto mt-[50px] xl:mt-52 block md:py-3 p-2 bg-white text-orange-500 font-semibold text-[10px] md:text-[13px] lg:text-base  xl:px-14">Add to cart</button>
+                </div>  
+                <div className={`${isOpenDescrMenu && activeProductId === id ? 'opacity-75 block' : 'hidden'} xl:block absolute inset-0 bg-gray-600 opacity-0  xl:hover:opacity-75 transition-opacity  text-white font-semibold p-2`}>
+                    {isOpenDescrMenu && <CgClose  onClick={() => setOpenDescrMenu(false)} className='cursor-pointer w-[22px] h-[22px] xl:w-[30px] xl:h-[30px] ml-auto'/>}
+                    <button onClick={() => {
+                        handleCopyLink('Added to cart')
+                        handleAddToCart(currentProduct)
+                        }}
+                         className="hover:text-white hover:bg-orange-500 transition-all duration-300 my-0 mx-auto mt-[30px] xl:mt-44 block md:py-3 p-2 bg-white text-orange-500 font-semibold text-[10px] md:text-[13px] lg:text-base  xl:px-14">Add to cart</button>
                     <div className="mt-6 flex flex-col xl:flex-row xl:gap-0 gap-3 items-center justify-evenly">
                         <div onClick={toggleOpenModal} className='flex items-center gap-1 text-[10px] md:text-base cursor-pointer hover:scale-105 transition-all duration-300'><BsFillShareFill size='12' color='#fff'/>Share</div>
                         {isOpenShareModal && (
@@ -92,18 +106,25 @@ const handleCopyLink = () => {
                                 </div>
                                 <div className='text-center flex flex-col gap-4 text-xs xl:text-base'>
                                 Copy link to clipboard
-                                <AiFillCopy onClick={handleCopyLink} className='block mx-auto cursor-pointer hover:scale-110 duration-200 w-[22px] h-[22px] xl:w-[50px] xl:h-[50px]'/>
+                                <AiFillCopy onClick={() => handleCopyLink('Url copied to clipboard')} className='block mx-auto cursor-pointer hover:scale-110 duration-200 w-[22px] h-[22px] xl:w-[50px] xl:h-[50px]'/>
                                 </div>
-                                {copied && (
-                                    <div className='fixed bottom-4 left-4 bg-white border border-black text-black rounded-[10px] px-2 py-4 '>Url copied to clipboard</div>
-                                )}
+                                
                             </motion.div>
                         )}
-                        <div onClick={() => handleAddToComparing(currentProduct)} className='flex items-center text-[10px] md:text-base gap-1 cursor-pointer hover:scale-105 transition-all duration-300'><MdCompareArrows size='20' color='#fff'/>Compare</div>
-                        <div onClick={() => handleAddToFavorite(currentProduct)} className='flex items-center text-[10px] md:text-base gap-1 cursor-pointer hover:scale-105 transition-all duration-300'><AiOutlineHeart color='#fff'/>Like</div>
+                        <div onClick={() => {
+                            handleCopyLink('Added for comparison')
+                            handleAddToComparing(currentProduct)
+                        }} className='flex items-center text-[10px] md:text-base gap-1 cursor-pointer hover:scale-105 transition-all duration-300'><MdCompareArrows size='20' color='#fff'/>Compare</div>
+                        <div onClick={() => {
+                            handleCopyLink('Added to favorite')
+                            handleAddToFavorite(currentProduct)
+                        }} className='flex items-center text-[10px] md:text-base gap-1 cursor-pointer hover:scale-105 transition-all duration-300'><AiOutlineHeart color='#fff'/>Like</div>
                     </div>
                     <Link to={`/shop/${id}`} className="text-center mx-auto p-2 xl:mt-16 text-[10px] md:text-base mt-5 border border-white block w-28 md:w-28 md:py-3 hover:scale-105 transition-all duration-300">Show More</Link>
                 </div>
+                {copied && (
+                    <div className='fixed bottom-4 left-2 md:left-4 font-semibold bg-white border md:border-[2px] text-[10px] md:text-base border-black text-black  p-2 md:px-2 md:py-4 z-50'>{copied}</div>
+                )}
             </motion.div>
         )
         })}
